ed8ce6c81ebabecd5d534c0d7ebe6a091035a8e2
Mattias Gees <mattias.gees@gmail.com>
2016-11-10 16:20:27 +0100

    Do not standard allow puppet connections to the outside

--- securitygroups/all.tf
+++ securitygroups/all.tf
@@ -41,13 +41,3 @@ resource "aws_security_group_rule" "sg_bastion_out_https" {
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
 }
-
-# Allow puppet connections to the outside
-resource "aws_security_group_rule" "sg_bastion_out_puppet" {
-  type              = "egress"
-  security_group_id = "${aws_security_group.sg_all.id}"
-  from_port         = 8140
-  to_port           = 8140
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
