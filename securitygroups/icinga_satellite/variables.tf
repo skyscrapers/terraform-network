a05640ea32e0479ee0c9e3dae2ed979a9a1a1bb8
iuri aranda <iuri.aranda@gmail.com>
2017-07-31 17:13:24 +0200

    Allow nrpe traffic from icinga satellite to internal instances

--- securitygroups/icinga_satellite/variables.tf
+++ securitygroups/icinga_satellite/variables.tf
@@ -14,3 +14,8 @@ variable "project" {
 variable "icinga_master_ip" {
   description = "IP of the Icinga master, in CIDR/32 notation"
 }
+
+variable "internal_sg_id" {
+  description = "The Icinga satellite will be able to access this security group through NRPE, if provided."
+  default     = ""
+}
