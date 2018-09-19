66afc50d33f1343b8a5ac419bee234b637811037
iuri aranda <iuri.aranda@gmail.com>
2018-09-19 10:14:57 +0200

    Add the option to provide extra tags for NAT gateways (#20)

--- nat_gateway/main.tf
+++ nat_gateway/main.tf
@@ -13,9 +13,7 @@ resource "aws_nat_gateway" "gateway" {
   allocation_id = "${aws_eip.nat_gateway.*.id[count.index]}"
   subnet_id = "${var.public_subnets[count.index]}"
 
-  tags {
-    Name = "${data.aws_subnet.ngw_subnets.*.availability_zone[count.index]}"
-  }
+  tags = "${merge(var.tags, map("Name", data.aws_subnet.ngw_subnets.*.availability_zone[count.index]))}"
 }
 
 resource "aws_route" "r" {
