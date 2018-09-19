cff9433bdfb3ee697722219678aaa5275f9e106b
iuri aranda <iuri.aranda@gmail.com>
2018-09-19 09:47:39 +0200

    Set the az in the nat gateways name for easier identification (#19)

--- nat_gateway/main.tf
+++ nat_gateway/main.tf
@@ -3,10 +3,19 @@ resource "aws_eip" "nat_gateway" {
   vpc = true
 }
 
+data "aws_subnet" "ngw_subnets" {
+  count = "${var.number_nat_gateways}"
+  id    = "${var.public_subnets[count.index]}"
+}
+
 resource "aws_nat_gateway" "gateway" {
   count="${var.number_nat_gateways}"
   allocation_id = "${aws_eip.nat_gateway.*.id[count.index]}"
   subnet_id = "${var.public_subnets[count.index]}"
+
+  tags {
+    Name = "${data.aws_subnet.ngw_subnets.*.availability_zone[count.index]}"
+  }
 }
 
 resource "aws_route" "r" {
